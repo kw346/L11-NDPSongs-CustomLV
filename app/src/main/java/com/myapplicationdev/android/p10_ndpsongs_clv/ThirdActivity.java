8311a39603ce9b0bf5f1d60d2bf50ddd34017836
kw346 <20022735@myrp.edu.sg>
2021-08-03 18:58:55 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/p10_ndpsongs_clv/ThirdActivity.java
@@ -0,0 +1,105 @@
+package com.myapplicationdev.android.p10_ndpsongs_clv;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    EditText etID, etTitle, etSingers, etYear;
+    Button btnCancel, btnUpdate, btnDelete;
+    RatingBar rb3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        setTitle(getTitle().toString() + " ~ " + getResources().getText(R.string.title_activity_third));
+
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        etID = (EditText) findViewById(R.id.etID);
+        etTitle = (EditText) findViewById(R.id.etTitle);
+        etSingers = (EditText) findViewById(R.id.etSingers);
+        etYear = (EditText) findViewById(R.id.etYear);
+
+        rb3 = findViewById(R.id.ratingBar3);
+
+        Intent i = getIntent();
+        final Song currentSong = (Song) i.getSerializableExtra("song");
+
+        etID.setText(currentSong.getId()+"");
+        etTitle.setText(currentSong.getTitle());
+        etSingers.setText(currentSong.getSingers());
+        etYear.setText(currentSong.getYearReleased()+"");
+        int rating = (int) rb3.getRating();
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(ThirdActivity.this);
+                currentSong.setTitle(etTitle.getText().toString().trim());
+                currentSong.setSingers(etSingers.getText().toString().trim());
+                int year = 0;
+                int rating = 0;
+                try {
+                    year = Integer.valueOf(etYear.getText().toString().trim());
+                } catch (Exception e){
+                    Toast.makeText(ThirdActivity.this, "Invalid year", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                currentSong.setYearReleased(year);
+
+                currentSong.setStars(rating);
+                int result = dbh.updateSong(currentSong);
+                if (result>0){
+                    Toast.makeText(ThirdActivity.this, "Song updated", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent();
+                    setResult(RESULT_OK);
+                    finish();
+                } else {
+                    Toast.makeText(ThirdActivity.this, "Update failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(ThirdActivity.this);
+                int result = dbh.deleteSong(currentSong.getId());
+                if (result>0){
+                    Toast.makeText(ThirdActivity.this, "Song deleted", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent();
+                    setResult(RESULT_OK);
+                    finish();
+                } else {
+                    Toast.makeText(ThirdActivity.this, "Delete failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+
+}
